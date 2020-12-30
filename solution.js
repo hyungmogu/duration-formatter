288956b08dec3a32cf4edce78b55aea0d673436f
Hyungmo Gu <guhyungm7@gmail.com>
2020-12-30 12:07:49 +0900

    first commit

---
+++ solution.js
@@ -0,0 +1,6 @@
+function formatDuration(seconds) {
+    return answer;
+}
+
+console.log(formatDuration(62))    // returns "1 minute and 2 seconds"
+console.log(formatDuration(3662))  // returns "1 hour, 1 minute and 2 seconds"
